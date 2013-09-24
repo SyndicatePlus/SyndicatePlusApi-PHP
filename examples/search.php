4349b0cb29c1c5d0987afcae33e5806be1ffc79a
Rogier Pennink <rogierpennink@gmail.com>
2013-09-24 10:00:16 +0200

    Updated search sample

--- examples/search.php
+++ examples/search.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This SyndicatePlus Api examples searches the SyndicatePlus API for
- * for the popular Dutch beer brand called "Grolsch" and displays the
+ * for the popular Dutch beer brand called "Heineken" and displays the
  * results as simple HTML.
  */
 
@@ -14,8 +14,17 @@ require_once "../src/productsapi.class.php";
 // it defaults to the latest version of the API
 $api = new SyndicatePlus\Api\ProductsApi(APIKEY, APISECRET);
 
-$results = $api->search("Grolsch");
+?><h1>Results for "Heineken"</h1><?php
+
+$results = $api->search("Heineken");
 foreach ( $results as $product ) {
-	var_dump($product);
+	?>
+	<img height="80" style="float: left; margin: 10px;" src="<?php echo $product->ImageUrl; ?>" />
+	<span>Product: <?php echo $product->Name; ?></span><br />
+	<span>Brand: <?php echo $product->Brand->Name; ?></span><br />
+	<span>Manufacturer: <?php echo $product->Manufacturer->Name; ?></span>
+	<div style="clear: both;"></div>
+	<hr />
+	<?php
 }
 ?>
